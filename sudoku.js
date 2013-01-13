85b58b6ae745be18144768e138fc74f8f9365954
quasimoto3000 <rishi.sadhir@gmail.com>
2013-01-13 17:29:08 -0500

    first commit

---
+++ sudoku.js
@@ -0,0 +1,10 @@
+// Author: Rishi Sadhir
+// File:   sudoku.js
+// Desc:   Initial Board for JavaScript Sudoku 
+//
+
+var initBoard = new Array(new Array(0, 0, 0, 1),
+                          new Array(0, 0, 0, 0),
+                          new Array(3, 0, 0, 0),
+                          new Array(0, 0, 2, 0)
+);
